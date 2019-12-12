033f75b34814bfc87e78101f6bd5dbe0a7714a14
Miles Valdez <momomilestv@gmail.com>
2019-12-12 13:30:37 -0800

    WIP

--- TimotheeMain/timothee.py
+++ TimotheeMain/timothee.py
@@ -1,18 +1,10 @@
 from redbot.core import commands
 
-<<<<<<< Updated upstream
-class frc(commands.Cog):
-    """My custom cog"""
-
-    @commands.command()
-    async def frc(self, ctx):
-=======
 class timothee(commands.Cog):
     """My custom cog"""
 
     @commands.command()
     async def timothee(self, ctx):
->>>>>>> Stashed changes
         """This does stuff!"""
         # Your code will go here
         await ctx.send("This is a test frc command! If you see this it works!")
