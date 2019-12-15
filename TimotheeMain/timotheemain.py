9c6f2996140867b8f8af755a12acd50df5eb0762
Miles valdez <momomilestv@gmail.com>
2019-12-15 15:05:29 -0800

    File rename

--- TimotheeMain/timotheemain.py
+++ TimotheeMain/timotheemain.py
@@ -1,13 +1,7 @@
 from redbot.core import commands
 
-class Mycog(commands.Cog):
+class TimotheeMain(commands.Cog):
     """My custom cog"""
-
-    @commands.command()
-    async def mycom(self, ctx):
-        """This does stuff!"""
-        # Your code will go here
-        await ctx.send("I can do stuff!")
 
     @commands.command()
     async def beautiful(self, ctx):
